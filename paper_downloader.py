a6974ea7cf2f186720c19cb200c4b48956d98067
CTugTEZ <c.tugrultezcan@gmail.com>
2020-04-27 13:44:27 +0300

    Made improvements about naming

--- paper_downloader.py
+++ paper_downloader.py
@@ -14,18 +14,39 @@ class paper_download:
 
         list_file = open(list_file_name, "r")
         pdf_links = []
+        all_file = list_file.readlines()
 
-        # first 7 character must be: "    PDF"
-        for lines in list_file.readlines():
+
+        for a, lines in enumerate(all_file):
             if lines[:7] == "    PDF":
                 pdf_links.append(lines.split('\"')[1]) # splitting just for link
+            if lines[:7] == "\"title\"":
+                if len(lines.split("\"")[3]) != 2:
+                    pdf_links.append(lines.split("\"")[3])
+            if lines[:11] == "\"published\"":
+                pdf_links.append(lines.split("\"")[3][:4])
+            if lines[:9] == "\"authors\"":
+                pdf_links.append(all_file[a+1].split("\"")[1])
 
-        self.printProgressBar(0,len(pdf_links),prefix='Progress:', suffix='Complete', length=50)
 
-        for i, pdfs in enumerate(pdf_links):
+        year, title, authors, link = [], [], [], []
+        for num1 in range(0,4,1):
+            for line_num in range(num1, len(pdf_links), 4):
+                if num1 == 0:
+                    title.append(pdf_links[line_num])
+                elif num1 == 1:
+                    year.append(pdf_links[line_num])
+                elif num1 == 2:
+                    authors.append(pdf_links[line_num])
+                elif num1 == 3:
+                    link.append(pdf_links[line_num])
 
-            self.printProgressBar(i+1,len(pdf_links),prefix='Progress:', suffix='Complete', length=50)
-            file_name_path = args.path+pdfs.split("/")[-1]+".pdf"
+
+        self.printProgressBar(0,len(link),prefix='Progress:', suffix='Complete', length=50)
+
+        for i, pdfs in enumerate(link):
+            self.printProgressBar(i+1,len(link),prefix='Progress:', suffix='Complete', length=50)
+            file_name_path = args.path[0:-1]+"/"+str(year[i])+"_"+str(title[i])+"_"+str(authors[i])+".pdf"
             r = requests.get(pdfs, stream=True)
             with open(file_name_path, "wb") as pdf:
                 for chunk in r.iter_content(chunk_size=1024):
